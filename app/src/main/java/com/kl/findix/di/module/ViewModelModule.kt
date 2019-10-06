75b77b33f3581b5c59701a1721e772f3f4676500
Katsumi Kusumi <katsumi.kusumi.ph@gmail.com>
2019-10-06 22:43:10 +0900

    MapをFragment化。 DrawerNavigation削除。

--- app/src/main/java/com/kl/findix/di/module/ViewModelModule.kt
+++ app/src/main/java/com/kl/findix/di/module/ViewModelModule.kt
@@ -8,27 +8,32 @@ import com.kl.findix.ui.profile.ProfileViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
+import javax.inject.Singleton
 
 @Module
 abstract class ViewModelModule {
 
     @Binds
     @IntoMap
+    @Singleton
     @ViewModelKey(LoginViewModel::class)
     internal abstract fun provideLoginViewModel(loginViewModel: LoginViewModel): ViewModel
 
     @Binds
     @IntoMap
+    @Singleton
     @ViewModelKey(MapsViewModel::class)
     internal abstract fun provideMapViewModel(mapsViewModel: MapsViewModel): ViewModel
 
     @Binds
     @IntoMap
+    @Singleton
     @ViewModelKey(ProfileViewModel::class)
     internal abstract fun provideProfileViewModel(profileViewModel: ProfileViewModel): ViewModel
 
     @Binds
     @IntoMap
+    @Singleton
     @ViewModelKey(SignUpViewModel::class)
     internal abstract fun provideSignUpViewModel(signUpViewModel: SignUpViewModel): ViewModel
 
---
+++ app/src/main/java/com/kl/findix/navigation/MapsNavigator.kt
@@ -0,0 +1,11 @@
+package com.kl.findix.navigation
+
+import androidx.navigation.NavController
+import com.kl.findix.ui.login.LoginFragmentDirections
+
+class MapsNavigator (
+    private val navController: NavController
+) {
+
+
+}
