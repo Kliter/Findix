b677ab1d0d13987e1a03d6f8c14ab292a800ed6e
Katsumi Kusumi <katsumi.kusumi.ph@gmail.com>
2019-12-08 15:45:02 +0900

    [Build] ProfileImage未登録時にDroidくんを表示できるよう修正

--- app/src/main/java/com/kl/findix/util/ViewExt.kt
+++ app/src/main/java/com/kl/findix/util/ViewExt.kt
@@ -2,10 +2,23 @@ package com.kl.findix.util
 
 import android.graphics.Bitmap
 import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.databinding.BindingAdapter
+import com.kl.findix.R
 
 @BindingAdapter("app:bitmapSrc")
-fun ImageView.setBitmapSrc(bitmap: Bitmap) {
-    this.setImageBitmap(bitmap)
+fun ImageView.setBitmapSrc(bitmap: Bitmap?) {
+    if (bitmap == null) {
+        this.setImageBitmap(
+            ResourcesCompat.getDrawable(
+                resources,
+                R.mipmap.ic_launcher,
+                null
+            )?.toBitmap()
+        )
+    } else {
+        this.setImageBitmap(bitmap)
+    }
 }
 
